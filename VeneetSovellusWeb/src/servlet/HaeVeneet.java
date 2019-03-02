fef105693a61d02e037aca0c70f10844679ffcb8
freyenne <46966218+freyenne@users.noreply.github.com>
2019-03-02 13:17:55 +0200

    Update HaeVeneet.java

--- VeneetSovellusWeb/src/servlet/HaeVeneet.java
+++ VeneetSovellusWeb/src/servlet/HaeVeneet.java
@@ -36,12 +36,12 @@ public class HaeVeneet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("HaeVeneet.doPost()");
 		String hakusana = request.getParameter("hakusana");
-		/*
+		
 		if(hakusana.isEmpty()){
 			response.sendRedirect("haeveneet.jsp?ilmo=Anna hakusana");
 			return;
 		}
-		*/
+		
 		Dao dao = new Dao();
 		ArrayList<Vene> veneet = dao.listaaKaikki(hakusana);
 		request.setAttribute("veneet", veneet);		
